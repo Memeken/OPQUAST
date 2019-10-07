48c003b4dcbbeda36fa56c64ca16018c05edba89
Memeken <coralie2319@hotmail.fr>
2019-10-07 14:07:09 +0200

    first commit

---
+++ opquast.php
@@ -0,0 +1,16 @@
+<?php
+include "glossaire.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	var_dump($glossaire);
+	?>
+</body>
+</html>
