cd6791b94058245b82a577685593a617e05bd840
Memeken <coralie2319@hotmail.fr>
2019-10-07 22:25:42 +0200

    modification du 7/10 soir

--- opquast.php
+++ opquast.php
@@ -7,10 +7,15 @@ include "glossaire.php";
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
+	<h1>&rarr; GLOSSAIRE OPQUAST	&larr;</h1>
+	<br>
 	<a href="" title="Rafraichir..."><input type="button" name="newdef" value="Glossaire aléatoire"> </a>
 	<br>
+	
+	<div id="titre">
 	<?php
 	// var_dump($glossaire);
 	//int count ( mixed $glossaire [, int $mode= COUNT_NORMAL ] )
@@ -19,14 +24,22 @@ include "glossaire.php";
 	//echo $glossaire{rand(0,148)};
 	//echo '<pre>';
 	$index = rand(0, count($glossaire) - 1);
-	echo $glossaire[$index]['title'] . "<br>";
+	echo $glossaire[$index]['title'] . "<br>" . "<br>";
+	?>
+	</div>
+	<br>
+	<div id="contenu">
+	<?php
 	echo $glossaire[$index]['description'];
 	echo "<br>";
-	echo $glossaire[1]['title'];
+	//echo $glossaire[1]['title'];
 
 	//echo $glossaire[rand(0,148)]['title'];
 
 	//echo '</pre>';
 	?>
+	</div>
+	<br>
+	
 </body>
 </html>
